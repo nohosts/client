5ade996d59946f6de7a6340f96418ddd1d926f3a
胡青杨 <i@do.codes>
2017-12-28 15:52:46 +0800

    Feature: Spawn in Electron

--- app/main.js
+++ app/main.js
@@ -3,11 +3,6 @@ const path = require('path');
 const url = require('url');
 const cp = require('child_process');
 const { createWindow } = require('./lib/util');
-const { override } = require('./lib/electron-child-process');
-
-
-// Override functions in child-process module
-override(cp);
 
 
 let win;
