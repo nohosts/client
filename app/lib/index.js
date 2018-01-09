79ae7d971e73de6c9e450c7343db11d4c7293a46
胡青杨 <i@do.codes>
2018-01-09 16:19:05 +0800

    Compatible with macOS

--- app/lib/index.js
+++ app/lib/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const lan = require('/Users/huqingyang/Desktop/lan-settings');
+const lan = require('lan-settings');
 const startWhistle = require('whistle');
 const { getPort } = require('./util');
 
